0a46d54ce1e056f299d0a381ed4644b2e4902d0f
downsd16 <downsd16@gmail.com>
2023-09-20 20:33:11 -0400

    merge

--- client/tech-amelia/src/app/services/authentication-service.service.ts
+++ client/tech-amelia/src/app/services/authentication-service.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationServiceService {
 
-  constructor() { }
+  constructor(private msalService: MsalService) {}
+
+  login() {
+    this.msalService.loginPopup().subscribe((response) => {
+      console.log(response);
+      this.msalService.instance.setActiveAccount(response.account);
+    });
+  }
+
+  logout(): void {
+    this.msalService.logout();
+  }
+
+  getAccessToken(): string {
+    return this.msalService.acquireTokenSilent({
+      scopes: ['User.Read', 'openid', 'profile'],
+    }).subscribe((response) => {
+      return response.accessToken;
+    }
+  }
 }
