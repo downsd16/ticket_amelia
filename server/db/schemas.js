7b412c9fceb7ef6225d1d0fa9be3cda8b2203bed
Devin Downs <downsd16@gmail.com>
2023-09-22 21:32:45 +0000

    update schema

--- server/db/schemas.js
+++ server/db/schemas.js
@@ -2,5 +2,15 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 let ticketSchema = new Schema({
-    
+    name: String,
+    tech: String,
+    date: { type: Date, default: Date.now },
+    issue: String,
+    comments: [{ body: String, date: Date , tech: String }],
+    total: Number,
+    sys_info: [{
+        make: { type: String, enum: ['Acer','Asus','Dell','Lenovo','HP','Other'], required: true },
+        model: String,
+        
+    }]
 })
