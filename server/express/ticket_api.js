08b8fd05245da16540a636477e00de5d2ddfa3ec
Devin Downs <downsd16@gmail.com>
2023-09-26 12:54:09 +0000

    add express to stack and add getters

---
+++ server/express/ticket_api.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+// Get Active Tickets
+app.get('/', function (req, res) {
+
+})
+
+// Get All Tickets
+app.get('/', function (req, res) {
+
+})
+
+//Get Ticket for Technician
+app.get('/', function(req, res){
+    
+})
+
+// Get All Clients
+app.get('/', function (req, res) {
+
+})
+
+// Get Active Tickets
+app.get('/', function (req, res) {
+
+})
+
+app.listen(3000)    //change default port for kubernetes service
